b21becca9151b524cc43691132b88e7351b295b4
Boris Ts. (cborka) <cborka777@yandex.ru>
2023-03-04 10:56:41 +0700

    Работа с файлами

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+// Подключение стандартных модулей
 const os = require('os');
 
 let res = os.platform();
 console.log(res)
 
+// Свои модули
+
 const cbw = require('./cbw');
 
 console.log(cbw.add(3, 4));
 console.log(cbw.hi)
+
+// Работа с файлами
+
+console.log('Работа с файлами');
+
+const fs = require('fs');
+
+// Синхронно
+fs.writeFileSync('some_text.txt', 'Записано функцией writeFileSync');
+res = fs.readFileSync('some_text.txt', 'utf-8');
+console.log(res);
+
+// Асинхронно
+fs.writeFile('some_text.txt', 'Записано функцией writeFile', (err, data) => {
+  fs.readFile('some_text.txt', 'utf-8', (err, data) => {
+    console.log(data);
+  })
+  console.log('Reading...');
+})
