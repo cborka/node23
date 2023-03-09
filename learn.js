a2d31d88293dbb0959b1a769dfd4e63b1f857c7c
Boris Ts. (cborka) <cborka777@yandex.ru>
2023-03-09 17:06:16 +0700

    230309 учу JS

---
+++ learn.js
@@ -0,0 +1,12 @@
+function alert(msg) {
+  console.log(msg);
+}
+
+console.log('Learn.js!');
+alert("Alert!")
+
+const bigInt = 1234567890123456789012345678901234567890n;
+alert(bigInt + 3n);
+
+let isGreater = 4 > 1;
+alert( isGreater );
