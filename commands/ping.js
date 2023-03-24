f14e376c6a502352ba0160b86bc3addea59dbcf0
Boris Ts. (cborka) <cborka777@yandex.ru>
2023-03-24 17:25:38 +0700

    2023-03-24 discord.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
+//    await console.log('Pong!');
 		await interaction.reply('Pong!');
 	},
 };
